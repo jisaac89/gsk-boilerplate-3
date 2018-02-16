32e1628c8c0aa75da03e9e27d0d9a9bc593b0664
jisaac89 <josephisaac89@gmail.com>
2018-02-15 18:39:49 -0800

    update menupane

--- src/components/navigation/MenuPane.tsx
+++ src/components/navigation/MenuPane.tsx
@@ -34,39 +34,38 @@ class MenuPane extends React.Component<IMenuPaneProps, any> {
 
         return (
             <SlideIn className="z3" if={appStore.menu} from="bottom" fill>
-
-                <Layer scrollY id="main" flexCenter={!appStore.mobile} fill className="text-left pt50" theme="light">
-                    <div className="p20 w500px center-width">
-                        <img className="profile-pic pull-left mb40" src="https://www.lawlogix.com/wp-content/uploads/2015/05/LW-603-p28-partner-profile.jpg" />
-                        <div className="pull-left dinblock mt20 ml20">
-                            <h1>Welcome back,</h1>
-                            <h2>Stokes</h2>
+                <Layer fill flex>
+                    <Layer scrollY id="main" flexCenter={!appStore.mobile} fill className="text-left pt50" theme="light">
+                        <div className="p20 w500px center-width">
+                            <img className="profile-pic pull-left mb40" src="https://www.lawlogix.com/wp-content/uploads/2015/05/LW-603-p28-partner-profile.jpg" />
+                            <div className="pull-left dinblock mt20 ml20">
+                                <h1>Welcome back,</h1>
+                                <h2>Stokes</h2>
+                            </div>
+                            <Toolbar block className="center-width text-left" spacing vertical>
+                                <Emerge enter="fadeIn" if={appStore.menu}>
+                                    <div></div>
+                                    <Toolbar block flush flex className="mb20">
+                                        {labResultsStore.list.length ? <Button theme="error">{labResultsStore.list.length}</Button> : null}
+                                        <RouterButton loading={labResultsStore.loading} tabIndex={-1} block size="large" history={history} route="/labResults" title="Lab Results" />
+                                    </Toolbar>
+                                    <Toolbar block flush flex className="mb20">
+                                        {prescriptionsStore.prescriptions.length ? <Button theme="error">{prescriptionsStore.prescriptions.length}</Button> : null}
+                                        <RouterButton loading={prescriptionsStore.loading} block size="large" history={history} route="/prescriptions" title="Prescriptions" />
+                                    </Toolbar>
+                                    <Toolbar block flush flex className="mb20">
+                                        <Button theme="error">1</Button>
+                                        <RouterButton block size="large" history={history} route="/discounts" title="Discounts" />
+                                    </Toolbar>
+                                    <RouterButton className="mb20" disabled block size="large" history={history} route="/" title="My Profile" />
+                                    <RouterButton disabled block size="large" history={history} route="/" title="Settings" />
+                                </Emerge>
+                            </Toolbar>
                         </div>
-                        <Toolbar block className="center-width text-left" spacing vertical>
-                            <Emerge enter="fadeIn" if={appStore.menu}>
-                                <div></div>
-                                <Toolbar block flush flex className="mb20">
-                                    {labResultsStore.list.length ? <Button theme="error">{labResultsStore.list.length}</Button> : null}
-                                    <RouterButton loading={labResultsStore.loading} tabIndex={-1} block size="large" history={history} route="/labResults" title="Lab Results" />
-                                </Toolbar>
-                                <Toolbar block flush flex className="mb20">
-                                    {prescriptionsStore.prescriptions.length ? <Button theme="error">{prescriptionsStore.prescriptions.length}</Button> : null}
-                                    <RouterButton loading={prescriptionsStore.loading} block size="large" history={history} route="/prescriptions" title="Prescriptions" />
-                                </Toolbar>
-                                <Toolbar block flush flex className="mb20">
-                                    <Button theme="error">1</Button>
-                                    <RouterButton block size="large" history={history} route="/discounts" title="Discounts" />
-                                </Toolbar>
-                                <RouterButton className="mb20" disabled block size="large" history={history} route="/" title="My Profile" />
-                                <RouterButton disabled block size="large" history={history} route="/" title="Settings" />
-                            </Emerge>
-                        </Toolbar>
-                    </div>
-                    <SlideIn if={true} from="bottom">
-                        <Layer theme="e-NightMode" className="border-top text-center p10">
-                            <Button icon="unlock" onClick={this.signOut.bind(this)} size="large" block>Sign Out</Button>
-                        </Layer>
-                    </SlideIn>
+                    </Layer>
+                    <Layer theme="e-NightMode" className="text-center p10">
+                        <Button icon="unlock" onClick={this.signOut.bind(this)} size="large" block>Sign Out</Button>
+                    </Layer>
                 </Layer>
             </SlideIn>
         )
